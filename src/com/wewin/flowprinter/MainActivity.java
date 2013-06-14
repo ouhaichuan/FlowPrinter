6b303b62eef513ec14907cd88f5d99823d2b04e3
ouhaichuan <531912856@qq.com>
2013-06-14 15:03:33 +0800

    fisrt commit
    Signed-off-by: ouhaichuan <531912856@qq.com>

---
+++ src/com/wewin/flowprinter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.wewin.flowprinter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private long exitTime = 0;// 退出倒计时
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK
+				&& event.getAction() == KeyEvent.ACTION_DOWN) {
+			if ((System.currentTimeMillis() - exitTime) > 2000) {
+				Toast.makeText(getApplicationContext(), "再按一次退出程序", 0).show();
+				exitTime = System.currentTimeMillis();
+			} else {
+				finish();
+				System.exit(0);
+			}
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+}
---
+++ src/com/wewin/flowprinter/WelcomeActivity.java
@@ -0,0 +1,41 @@
+package com.wewin.flowprinter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * 欢迎界面
+ * 
+ * @author HCOU
+ * @time 2013.05.27 17:25:00
+ */
+public class WelcomeActivity extends Activity {
+	private Handler mHandler;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome);
+		// 初始化界面
+		initView();
+	}
+
+	public void initView() {
+		mHandler = new Handler();
+		// 延迟3秒加载欢迎界面
+		mHandler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				goMainActivity();
+			}
+		}, 3000);
+	}
+
+	public void goMainActivity() {
+		Intent intent = new Intent();
+		intent.setClass(this, MainActivity.class);
+		startActivity(intent);
+		finish();// 将活动推向后台
+	}
+}
